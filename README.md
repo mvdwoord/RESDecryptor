628873694c0e9cd912cb372c2c0b0d20a7421e25
mvdwoord <maarten@vanderwoord.nl>
2017-01-05 12:04:38 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # RESDecryptor
-Tool for extracting passwords stored in RES (One) Automation (Manager)
+
+Tool for extracting secrets stored in RES (One) Automation (Manager)
+
+Internal encryption of secrets in RES is fairly straightforward, using a substitution cipher with a small number of hardcoded keys. This tool helps you decrypt secrets, either extracted from building blocks, from the datastore, or otherwise.
+
+For now the tool deals with secrets stored in __Task__ objects. Secrets from global variables are encrypted in a different manner.
+
+## Example
+
+```
+Python 3.6.0 (default, Jan  2 2017, 17:55:52)
+[GCC 4.2.1 Compatible Apple LLVM 8.0.0 (clang-800.0.42.1)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> from resdecryptor import *
+>>> my_key = cipher_keys["command"]
+>>> my_key
+'Grail'
+>>> my_string = 'All your base are belong to us.'
+>>> my_secret = encrypt(my_string, my_key)
+>>> my_secret
+'00B300CD00D5008C00C000E100D600DB008C00A900D300D400CE008C00A800E400C6008900CE00AC00DE00D000D700D3006700E600D0008900E100BA00A0'
+>>> decrypt(my_secret, my_key)
+'All your base are belong to us.'
+>>>
+```
+
+## To Do
+
+I would like to:
+
+- add (bulk) parsing / extraction from building block xml
+- complete list of task types / keys
+- add support for secrets in global variables
---
+++ resdecryptor.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+"""RES Decryptor helps with decrypting (or encrypting) secrets stored in RES (One) Automation (Manager)."""
+# import builtins
+# import 3rd party
+# Modify path
+# Authorship information
+__author__ = "Maarten van der Woord"
+__copyright__ = "Copyright 2007, Maarten van der Woord"
+__credits__ = ["Maarten van der Woord"]
+__license__ = "Unlicense"
+__version__ = "0.1"
+__maintainer__ = "Maarten van der Woord"
+__email__ = "maarten@vanderwoord.nl"
+__status__ = "Development"
+
+# Several cipher keys are in use, depending on the type of task the secret is stored in.
+# I have listed some, but the list can be extended somewhat.
+cipher_keys = {
+    "command": "Grail",
+    "filetask": "17FiLeVerSioN1988",
+    "deploytask": "77DepLoyComPoNent14",
+    "adtask": "ActiveDirectory",
+    "unknown": "Dune2",
+    "wfstudio": "R3SWFsTuD10",
+    "well...": "RES=Gold",
+    "maitask": "Send@Mail",
+    "sshtask": "SSHCommands",
+    "domaintask": "TaskDomain",
+    "webservice": "WebService"
+}
+
+
+def hexstring_to_wydelist(s):
+    """Takes hexadecimal string representation and breaks it up into a list of wydes.
+        This cold be factored into hextsinrg_to_intlist function but it is useful standalone
+        for exploring and debugging."""
+    n = 4
+    return list([s[i:i+n] for i in range(0, len(s), n)])
+
+
+def hexstring_to_intlist(s):
+    """Takes hexadecimal string representation and returns a list of numerical values (codepoints)."""
+    return map((lambda x: int(x, 16)), hexstring_to_wydelist(s))
+
+
+def encrypt(s, k):
+    """Encrypts string s with key k."""
+    encrypted_values = [(ord(c) + ord(k[(i + 1) % len(k)])) for i,c in enumerate(s)]
+    return "".join(map((lambda x: format(x, '04X')), encrypted_values))
+
+
+def decrypt(s, k):
+    """Decrypts secret s using key k."""
+    encrypted_values = hexstring_to_intlist(s)
+    decrypted_values = [(c - ord(k[(i + 1) % len(k)])) for i, c in enumerate(encrypted_values)]
+    return "".join(map((lambda x: chr(x)), decrypted_values))
